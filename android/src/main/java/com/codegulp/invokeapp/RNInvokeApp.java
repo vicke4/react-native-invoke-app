3060f24b7836040c3d5ce66e0de7be1efa9023be
Janaka <janakact@users.noreply.github.com>
2019-07-11 19:07:26 +0530

    Change flag to FLAG_ACTIVITY_NEW_TASK
    Fix for #3
    Tested in Android 5 and 7

--- android/src/main/java/com/codegulp/invokeapp/RNInvokeApp.java
+++ android/src/main/java/com/codegulp/invokeapp/RNInvokeApp.java
@@ -51,7 +51,7 @@ public class RNInvokeApp extends ReactContextBaseJavaModule {
             Class<?> activityClass = Class.forName(className);
             Intent activityIntent = new Intent(reactContext, activityClass);
 
-            activityIntent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             reactContext.startActivity(activityIntent);
         } catch(Exception e) {
             Log.e(LOG_TAG, "Class not found", e);
